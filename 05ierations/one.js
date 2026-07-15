a35c1b89da6fe7dfaf15893e47216a5c25022574
Devanshu lichade <devanshulichade44@gmail.com>
2026-07-15 02:41:10 +0000

    loop

---
+++ 05ierations/one.js
@@ -0,0 +1,52 @@
+// for 
+//for (let index = 0; index < array.length; index++) {
+   // const element = array[index];
+//}
+//for(i= 0 ; i<=10 ;i++){
+    //const element = i 
+    //console.log(element)
+//}
+//for (let i = 0; i < 10; i++) {
+  //  element = i
+    //if(element==5){
+       // console.log("5 is found")
+//    }
+  //  console.log(i)
+
+// }   
+// for (let i = 0; i < 10; i++) {
+    //console.log(`outer loop ${i}`)
+    //for (let j = 0; j < 10; j++) {
+        //console.log(`inner loop ${j} and outer loop ${i}`)
+
+
+   // }
+// }
+  //for (let i = 0; i <= 10; i++) {
+    //console.log(`outer loop ${i}`)
+    //for (let j = 0; j <= 10; j++) {
+    // console.log(`inner loop ${j} and outer loop ${i}`)
+       // console.log(i+"*"+j+"="+i*j)
+
+   // }
+  //}
+/* let myarray = ["flash","superman","batman","spiderman"]
+for (let i =0 ; i < myarray.length ; i++){
+    const element = myarray[i]
+    console.log(element)
+} */
+// break and continue
+for (let i = 0; i <= 10; i++) {
+    if (i == 5) {
+        console.log("5 is found")
+        break;
+    }
+    console.log(i)
+}
+for (let i = 0; i <= 10; i++) {
+    if (i == 5) {
+        console.log("5 is found")
+        continue;
+    }
+    console.log(i)
+}
---
+++ 05ierations/two.js
@@ -0,0 +1,17 @@
+/* let index =5
+while (index <= 10) {
+    console.log(index)
+    index = index + 5
+}
+let myarray = ["flash","superman","batman","spiderman"]
+let index1 = 0
+while (index1 < myarray.length) {
+    const element = myarray[index1]
+    console.log(element)
+    index1++
+} */
+let score = 11
+do{
+    console.log(score)
+    score++
+}while(score < 10);
